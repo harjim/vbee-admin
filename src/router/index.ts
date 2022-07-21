fffca7a86656955c2ec0e5d23e9c2a632630d58c
hzhoup <hzhouping@outlook.com>
2022-07-21 20:25:38 +0800

    feat: 配置路由跳转时显示 NProgress

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,8 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import NotFound from '/@/views/404.vue'
+import NProgress from 'nprogress'
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 
-const routes:Array<RouteRecordRaw> = [
+const routes: Array<RouteRecordRaw> = [
   {
     path: '/404',
     name: '404',
@@ -12,6 +13,16 @@ const routes:Array<RouteRecordRaw> = [
 const router = createRouter({
   history: createWebHistory(),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  NProgress.configure({ showSpinner: false })
+  NProgress.start()
+  next()
+})
+
+router.afterEach((to, from) => {
+  NProgress.done()
 })
 
 export default router
--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,6 +1,5 @@
 import Axios from 'axios'
 import { ElMessage } from 'element-plus'
-import NProgress from 'nprogress'
 
 const baseURL = 'https://api.github.com'
 
@@ -12,7 +11,6 @@ const axios = Axios.create({
 // 前置拦截器（发起请求之前的拦截）
 axios.interceptors.request.use(
   (response) => {
-    NProgress.start()
     /**
      * 根据你的项目实际情况来对 config 做处理
      * 这里对 config 不做任何处理，直接返回
@@ -20,7 +18,6 @@ axios.interceptors.request.use(
     return response
   },
   (error) => {
-    NProgress.done()
     return Promise.reject(error)
   }
 )
@@ -32,7 +29,6 @@ axios.interceptors.response.use(
      * 根据你的项目实际情况来对 response 和 error 做处理
      * 这里对 response 和 error 不做任何处理，直接返回
      */
-    NProgress.done()
     return response
   },
   (error) => {
@@ -44,7 +40,6 @@ axios.interceptors.response.use(
     } else {
       ElMessage.error(`${error}`)
     }
-    NProgress.done()
     return Promise.reject(error)
   }
 )
