31fe781ec56c9386e6ecd6e62f215ced60ed0d75
hzhoup <hzhouping@outlook.com>
2022-07-22 21:08:21 +0800

    feat: 删除模板 HelloWorld

--- src/store/globalSetting.ts
+++ src/store/globalSetting.ts
@@ -3,7 +3,8 @@ import { reactive } from 'vue'
 
 export const useGlobalSetting = defineStore('globalSetting', () => {
   const setting = reactive({
-    theme: 'dark'
+    theme: 'dark',
+    isMobile: false
   })
   const lang = reactive({
     // 默认语言，可选值<zh-cn|en>
@@ -14,6 +15,12 @@ export const useGlobalSetting = defineStore('globalSetting', () => {
       { name: 'zh-cn', value: '中文简体' },
       { name: 'en', value: 'English' }
     ]
+  })
+
+  watchEffect(() => {
+    setting.isMobile =
+      !!navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i)
+    console.log(setting.isMobile)
   })
 
   function setLang (val: string) {
